8657a8b851b9a1988072c1d90b96057f77ed0f2a
ivanh0929 <ivanhforbuisness@gmail.com>
2026-06-28 21:56:40 -0400

    Basic Movement + Camera + Cutscene Stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    /* TODO
+     * Add social stats
+     * Add Yollars
+     * Prevent inputs from being registered when in dialogue scene
+     * Add Interacts
+     * Add Attacks (Way Later)
+     */
+
+    [Header("References")]
+    CharacterController charaController;
+
+    [Header("Input Package System References")]
+    InputAction movement;
+    InputAction interact;
+    InputAction attack;
+    
+
+    [Header("Inputs")]
+    Vector2 movementInput;
+    // interact bool
+    // attack bool
+
+    [Header("Movement Settings")]
+    [SerializeField] float walkSpeed;
+    [SerializeField] float turnSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        charaController = GetComponent<CharacterController>();
+        movement = InputSystem.actions.FindAction("Move");
+        //interact = InputSystem.actions.FindAction("Interact");
+        //attack = InputSystem.actions.FindAction("Attack");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MovementUpdate();
+    }
+
+    void GetInputs()
+    {
+        movementInput = movement.ReadValue<Vector2>();
+    }
+
+    void MovementUpdate()
+    {
+        Movement();
+    }
+
+
+    void Movement()
+    {
+        // Movement
+        Vector3 fullMovement = new Vector3(movementInput.x, 0, movementInput.y);
+        fullMovement = fullMovement * walkSpeed;
+        charaController.Move(fullMovement);
+
+        // Turning
+
+        if (fullMovement != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(fullMovement.normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        }
+
+    }
+}
